8a7774256bc0e982fa7cab480043b2ec2c2fb465
Jorge William <jorge_william_cardos@yahoo.com.br>
2025-05-20 18:34:34 -0300

    feat: estrutura base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import DashboardPage from './pages/dashboard/dashboard.page'
+
+function App() {
+
+  return (
+    <>
+      <DashboardPage />
+    </>
+  )
+}
+
+export default App
